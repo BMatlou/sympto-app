96989c4088ca96cb4bdd2f51851fd8f6a58c85b8
BMatlou <bankies.matlou2@gmail.com>
2025-08-14 12:35:33 +0200

    Ensure vercel-build.sh is executable

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-chmod +x node_modules/.bin/vite
+set -e                 # Stop on any error
+echo "Setting executable permission for Vite..."
+chmod +x ./node_modules/.bin/vite
+echo "Running Vite build..."
 npx vite build
